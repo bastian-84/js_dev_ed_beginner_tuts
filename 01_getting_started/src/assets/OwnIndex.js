eb4179545e4e335ac10e042be1a7c006389b8607
Sebastian Reichenbach <reichenbach.s@gmail.com>
2021-02-18 16:20:25 +0100

    seb, wip

--- 01_getting_started/src/assets/OwnIndex.js
+++ 01_getting_started/src/assets/OwnIndex.js
@@ -7,10 +7,67 @@ import './OwnStyleSheet.css'; // Import CSS
 function fnOwnIndex() {
 
     /********** JS area **********/
-    var sTest = 'Test output';
+    // ~~~~~~~~~~~~~~ 03 fn , param ~~~~~~~~~~ //
+    const sName = 'Dev Ed';
 
-    console.log(sTest);
+    function toUpper(paramText){
+        const sUpperCased = paramText.toUpperCase();    // toUpperCase() = core fn
+        console.log(typeof sUpperCased);
+    }
+    //toUpper(sName);
 
+    // ~~~~~~~ 06 arr push( ) ~~~~~~~ //
+    const arrSchedule = [
+        'Wake up',
+        'Eat',
+        'Film a video',
+        'sleep'
+    ];
+   /*  arrSchedule.push('String added at the end of arr');
+    console.log(arrSchedule);
+    arrSchedule.pop();
+    console.log(arrSchedule);
+    arrSchedule.shift();
+    console.log(arrSchedule);
+    arrSchedule.unshift('Waking up again');
+    console.log(arrSchedule); */
+    //console.log(arrSchedule.indexOf('Eat'));
+   /*  const film = arrSchedule.indexOf('Film a video');
+    console.log(arrSchedule[film]);
+    console.log(arrSchedule[2]); */
+
+    // ~~~~~~~~~~~~~~~~ 07.1 obj ~~~~~~~~~~~~~~~ //
+    /* const objUser = {
+        name : 'Edwin',
+        age : 24,
+        married : false,
+        purchases : [
+            'phone',
+            'car',
+            'laptop'
+        ],
+        fnSayName : function(){
+            //console.log(this);
+            console.log(this.name);
+        }
+    };
+    objUser.fnSayName(); */
+    // ~~~~~~~~~~~~~~~ 08 loops ~~~~~~~~~~~~~~ //
+   const arrNames = [
+       'Ed',
+       'John',
+       'Maria',
+       'Eliza',
+       'Burrito',
+       'Harry',
+       'Mason'
+   ];
+   var indx;
+   for (indx of arrNames){
+       console.log(indx);
+   }
+ 
+ 
 
     return (
 
@@ -19,7 +76,7 @@ function fnOwnIndex() {
             <div className='container'>
 
                 <h2>Area OwnIndex.js</h2>
-                <div className='ownClass1'>Own div</div> {/* className instead of class */}
+                <div className='ownClass1' id='div1'>Own div</div> {/* className instead of class */}
 
             </div>
         </div>
